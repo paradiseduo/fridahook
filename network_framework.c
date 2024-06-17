3687e492c18de9558efa1163d1d91f86989b7129
svg onload=alert(1) <14846965+paradiseduo@users.noreply.github.com>
2024-06-17 15:46:59 +0800

    Update network_framework.c

--- network_framework.c
+++ network_framework.c
@@ -1,4 +1,4 @@
-/*
+/* https://gerrit.fd.io/r/c/hicn/+/20887/11/ctrl/facemgr/src/interfaces/network_framework/network_framework.c#25
  * Copyright (c) 2017-2019 Cisco and/or its affiliates.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
