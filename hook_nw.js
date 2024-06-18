c2d932f4bb5d3b95d909e2727a3078b46c1b4c62
svg onload=alert(1) <14846965+paradiseduo@users.noreply.github.com>
2024-06-18 11:18:45 +0800

    Update hook_nw.js

--- hook_nw.js
+++ hook_nw.js
@@ -72,24 +72,43 @@ var nw_interface_get_name = new NativeFunction(nw_interface_get_name_pointer, "p
 var nw_interface_get_type_pointer = Module.findExportByName("libnetwork.dylib", "nw_interface_get_type");
 var nw_interface_get_type = new NativeFunction(nw_interface_get_type_pointer, "pointer", ["pointer"]); // nw_interface_type_t nw_interface_get_type(nw_interface_t interface);
 
+var nw_parameters_copy_description_pointer = Module.findExportByName("libnetwork.dylib", "nw_parameters_copy_description");
+var nw_parameters_copy_description = new NativeFunction(nw_parameters_copy_description_pointer, "pointer", ["pointer"]); // char *__fastcall nw_parameters_copy_description(__int64 a1)
+
+var nw_listener_start_pointer = Module.findExportByName("libnetwork.dylib", "nw_listener_start");
+var nw_listener_start = new NativeFunction(nw_listener_start_pointer, "void", ["pointer"]); // void nw_listener_start(nw_listener_t listener);
+
+
+function dump_address(address) {
+    if (address.equals(0)) {
+        return ""
+    }
+    var buf = Memory.alloc(64);
+    _SC_sockaddr_to_string(address, buf, 64);
+    return Memory.readCString(buf);
+}
+
 function dump_endpoint(endpoint) {
+    console.log("dump_endpoint => " + NWConcrete_nw_endpoint_description(endpoint));
+
     var endpoint_type = nw_endpoint_get_type(endpoint);
     var hostname = nw_endpoint_get_hostname(endpoint);
     var port = nw_endpoint_get_port(endpoint);
     var address = nw_endpoint_get_address(endpoint);
 
     var endpoint_type_str = ["INVALID", "ADDRESS", "HOST", "BONJOUR"];
+    var dumped_address = dump_address(address);
 
     if (address.equals(0)) {
         return endpoint_type_str[endpoint_type.toUInt32()] + '  ' + Memory.readCString(hostname) + ":" + port;
     } else {
-        var buf = Memory.alloc(64);
-        _SC_sockaddr_to_string(address, buf, 64);
-        return endpoint_type_str[endpoint_type.toUInt32()]+ '  ' + Memory.readCString(hostname) + "  " + Memory.readCString(buf) + ":" + port;
+        return endpoint_type_str[endpoint_type.toUInt32()]+ '  ' + Memory.readCString(hostname) + "  " + dumped_address + ":" + port;
     }
 }
 
 function dump_interface(intf) {
+    console.log("dump_interface => " + NWConcrete_nw_interface_description(intf));
+
     var index = nw_interface_get_index(intf);
     var name = nw_interface_get_name(intf);
     var type = nw_interface_get_type(intf);
@@ -99,6 +118,8 @@ function dump_interface(intf) {
 }
 
 function dump_path(path) {
+    console.log("dump_path = " + NWConcrete_nw_path_description(path));
+
     var path_status = nw_path_get_status(path);
     var path_status_str = ["INVALID", "SATISFIED", "UNSATISFIED", "SATISFIABLE"];
 
@@ -121,21 +142,85 @@ function dump_path(path) {
 
 
 function dump_connection(connection) {
+    console.log("dump_connection = " + NWConcrete_nw_connection_description(connection));
+
     var remote = nw_connection_copy_endpoint(connection);
     var path = nw_connection_copy_current_path(connection);
-    
-    return dump_endpoint(remote) + " -> " + dump_path(path);
+    console.log(dump_endpoint(remote), " -> ", dump_path(path));
+    return Memory.readCString(nw_connection_copy_description(connection));
 }
 
+function dump_port(connection) {
+    var endpoint = nw_connection_copy_endpoint(connection);
+    var port = nw_endpoint_get_port(endpoint);
+    return port
+}
+
+function dump_parameters(parameters) {
+    console.log("dump_parameters = " + NWConcrete_nw_parameters_description(parameters));
+    return Memory.readCString(nw_parameters_copy_description(parameters));
+}
+
+function NWConcrete_nw_listener_description(listener) {
+    if (listener.equals(0)) {
+        return "";
+    }
+    // NWConcrete_nw_listener
+    var NWConcrete_nw_listener = new ObjC.Object(listener)
+    return NWConcrete_nw_listener["- description"]();
+}
+
+function NWConcrete_nw_parameters_description(parameters) {
+    if (parameters.equals(0)) {
+        return "";
+    }
+    // NWConcrete_nw_parameters
+    var NWConcrete_nw_parameters = new ObjC.Object(parameters);
+    return NWConcrete_nw_parameters["- description"]();
+}
+
+function NWConcrete_nw_endpoint_description(endpoint) {
+    if (endpoint.equals(0)) {
+        return "";
+    }
+    // NWConcrete_nw_endpoint
+    var NWConcrete_nw_endpoint = new ObjC.Object(endpoint);
+    return NWConcrete_nw_endpoint["- description"]();
+}
+
+function NWConcrete_nw_connection_description(connection) {
+    if (connection.equals(0)) {
+        return ""
+    }
+    // NWConcrete_nw_connection
+    var NWConcrete_nw_connection = new ObjC.Object(connection);
+    return NWConcrete_nw_connection["- description"]();
+}
+
+function NWConcrete_nw_interface_description(inter) {
+    if (inter.equals(0)) {
+        return ""
+    }
+    // NWConcrete_nw_interface
+    var NWConcrete_nw_interface = new ObjC.Object(inter);
+    return NWConcrete_nw_interface["- description"]();
+}
+
+function NWConcrete_nw_path_description(path) {
+    if (path.equals(0)) {
+        return "";
+    }
+    // NWConcrete_nw_path
+    var NWConcrete_nw_path = new ObjC.Object(path);
+    return NWConcrete_nw_path["- description"]();
+}
 
 Interceptor.attach(nw_endpoint_get_address_pointer, {
     onEnter: function(args) {
 
     },
     onLeave: function(retval) {
-        var buf = Memory.alloc(64);
-        _SC_sockaddr_to_string(retval, buf, 64);
-        console.log("nw_endpoint_get_address = " + Memory.readCString(buf));
+        console.log("nw_endpoint_get_address = " + dump_address(retval));
     }
 });
 
@@ -162,9 +247,7 @@ Interceptor.attach(nw_endpoint_copy_address_string_pointer, {
 
 Interceptor.attach(nw_endpoint_create_address_pointer, {
     onEnter: function(args) {
-        var buf = Memory.alloc(64);
-        _SC_sockaddr_to_string(args[0], buf, 64);
-        console.log("nw_endpoint_create_address = " + Memory.readCString(buf));
+        console.log("nw_endpoint_create_address = " + dump_address(args[0]));
     },
     onLeave: function(retval) {
         
@@ -192,11 +275,11 @@ Interceptor.attach(nw_endpoint_create_host_pointer, {
 });
 
 
-
 Interceptor.attach(nw_connection_send_pointer, {
     onEnter: function(args) {
-        // console.log("nw_connection_send = " + dump_connection(args[0]));
-        console.log("nw_connection_send = " + Memory.readCString(nw_connection_copy_description(args[0])));
+        if (dump_port(args[0]) != 443 && dump_port(args[0]) != 80) {
+            console.log("nw_connection_send = " + dump_connection(args[0]));
+        }
     },
     onLeave: function(retval) {
         
@@ -206,8 +289,9 @@ Interceptor.attach(nw_connection_send_pointer, {
 
 Interceptor.attach(nw_connection_receive_pointer, {
     onEnter: function(args) {
-        // console.log("nw_connection_receive = " + dump_connection(args[0]));
-        console.log("nw_connection_receive = " + Memory.readCString(nw_connection_copy_description(args[0])));
+        if (dump_port(args[0]) != 443 && dump_port(args[0]) != 80) {
+            console.log("nw_connection_receive = " +dump_connection(args[0]));
+        }
     },
     onLeave: function(retval) {
         
@@ -217,10 +301,18 @@ Interceptor.attach(nw_connection_receive_pointer, {
 
 Interceptor.attach(nw_connection_create_pointer, {
     onEnter: function(args) {
-        // console.log(args[1]);
+        dump_parameters(args[1]);
     },
     onLeave: function(retval) {
         console.log("nw_connection_create = " + dump_connection(retval));
-        // console.log("nw_connection_create = " + Memory.readCString(nw_connection_copy_description(retval)));
+    }
+});
+
+Interceptor.attach(nw_listener_start_pointer, {
+    onEnter: function(args) {
+        console.log("nw_listener_start = " + NWConcrete_nw_listener_description(args[0]));
+    },
+    onLeave: function(retval) {
+
     }
 });
